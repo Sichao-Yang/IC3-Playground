aad27cd6d46605d53f5c2f838f4e8616a85af46f
sichao-yang <13816901408@163.com>
2024-05-24 11:38:55 +0800

    modified newframe and base check

--- code/pdr/pdr.py
+++ code/pdr/pdr.py
@@ -1,12 +1,14 @@
 from z3 import *
 
+DEV = True
+
 
 # This helper class specifies a cube and a frame
 # in which it is reachable.
 class Cube(object):
-    def __init__(self, model, variable_lookup, i=None):
+    def __init__(self, model, variable_lookup, frame_id=None):
         self.values = [simplify(variable_lookup[str(v)] == model[v]) for v in model if str(v) in variable_lookup]
-        self.i = i
+        self.frame_id = frame_id
 
     @property
     def cube(self):
@@ -17,7 +19,7 @@ class Cube(object):
         return Or(*[Not(value) for value in self.values])
 
     def __repr__(self):
-        return "{" + ("" if self.i == None else str(self.i) + ": ") + str(self.values) + "}"
+        return "{" + ("" if self.frame_id == None else str(self.frame_id) + ": ") + str(self.values) + "}"
 
 
 class StackFrame(object):
@@ -44,17 +46,32 @@ class PDR(object):
         self.stack_trace = []
 
     # Finds a cube in ~post and the latest frame
-    def getBadCube(self):
+    def get_bad_cube(self, base):
         s = Solver()
-        s.add(And(Not(self.post), self.stack_frames[self.N].expression))
+        if base:
+            badstate_formula = And(self.stack_frames[self.N].expression, Not(self.post))
+        else:
+            if DEV:
+                # T&F&!p'
+                badstate_formula = And(
+                    self.trans, self.stack_frames[self.N].expression, Not(substitute(self.post, self.vTOp))
+                )
+            else:
+                # F&!p
+                badstate_formula = And(Not(self.post), self.stack_frames[self.N].expression)
+        s.add(badstate_formula)
         if s.check() == sat:
             return Cube(s.model(), self.variable_dict, self.N)
         return None
 
     def newFrame(self):
-        print(self.N)
+        print("Add new frame", self.N)
         sys.stdout.flush()
-        self.stack_frames.append(StackFrame())
+        if DEV:
+            newframe = StackFrame(self.post)  # F=p
+        else:
+            newframe = StackFrame()  # F=[]
+        self.stack_frames.append(newframe)
 
     # Checks whether a cube has been entirely blocked
     # in the given frame, only for performance
@@ -76,7 +93,7 @@ class PDR(object):
         return s.model()
 
     # Checks whether the we have found an inductive invariant
-    def induct(self):
+    def check_ind_invariant(self):
         for i, frame in enumerate(self.stack_frames[:-1]):
             check_frame = frame.expression
             s = Solver()
@@ -89,7 +106,7 @@ class PDR(object):
     # Attemps to block a cube recursively
     # Returns None if the cube was able to be blocked
     # Returns a stack trace if the cube cannot be blocked
-    def blockCube(self, cube):
+    def rec_block_cube(self, cube):
         # If solution is for frame 0, we have found a stack trace reaching ~post
         if cube.i == 0:
             return [cube]
@@ -108,25 +125,30 @@ class PDR(object):
                 candidate = {v: solution[v] for v in solution if str(v) in self.variable_dict}
                 candidateCube = Cube(candidate, self.variable_dict, cube.i - 1)
                 # Attempt to block this new candidate as well
-                trace = self.blockCube(candidateCube)
+                trace = self.rec_block_cube(candidateCube)
                 if trace:
                     return trace + [cube]
         return None
 
     # Main entry point of PDR
     def run(self):
-        print("Current frame")
+        # base case
+        cube = self.get_bad_cube(base=True)
+        if cube:
+            return False, [cube]
+        print("Passed base check: I&P")
         self.newFrame()
         while True:
-            cube = self.getBadCube()
+            cube = self.get_bad_cube(base=False)
             if cube:
-                trace = self.blockCube(cube)
-                if trace != None:
+                trace = self.rec_block_cube(cube)
+                if trace != None:  # found CTI
                     return False, trace
             else:
-                invarient = self.induct()
-                if invarient is not None:
-                    return True, invarient
+                # TODO: push forward?
+                invariant = self.check_ind_invariant()
+                if invariant is not None:
+                    return True, invariant
                 self.newFrame()
 
     @property
--- code/pdr/test_pdr.py
+++ code/pdr/test_pdr.py
@@ -2,15 +2,23 @@ from pdr import PDR
 from z3 import *
 import inspect
 
-def generate_variables(N): return [Bool(chr(ord('a') + i)) for i in range(N)]
-def prime(variables): return [Bool(var.__str__() + '\'') for var in variables]
+
+def generate_variables(N):
+    return [Bool(chr(ord("a") + i)) for i in range(N)]
+
+
+def prime(variables):
+    return [Bool(var.__str__() + "'") for var in variables]
+
+
 def print_and_write(file, string):
     file.write(string + "\n")
     print(string)
 
-def verify_program(title, variables, primes, init, trans, post, show_result = True, show_trans = True):
+
+def verify_program(expected, title, variables, primes, init, trans, post, show_result=True, show_trans=True):
     fname = inspect.stack()[1][3]
-    with open(fname + ".out", 'w') as f:
+    with open(fname + ".out", "w") as f:
         print_and_write(f, title)
         print_and_write(f, "---------------------------------------")
         print_and_write(f, "Init: " + str(init))
@@ -20,54 +28,91 @@ def verify_program(title, variables, primes, init, trans, post, show_result = Tr
         print_and_write(f, "Post:" + str(post))
         pdr = PDR(variables, primes, init, trans, post)
         sat, output = pdr.run()
+        if sat == expected:
+            print_and_write(f, f"Test passed on {fname}")
+        else:
+            raise ValueError(f"Test failed on {fname}")
         res_string = ("SAT\n" if sat else "UNSAT\n") + str(output)
         f.write(res_string + "\n")
         print(res_string if show_result else (("SAT\n" if sat else "UNSAT\n") + "Hidden result due to length"))
 
+
 def counter_unsat():
-    variables = [BitVec('x', 8)]
+    variables = [BitVec("x", 8)]
     x = variables[0]
-    primes = [BitVec('x\'', 8)]
+    primes = [BitVec("x'", 8)]
     xp = primes[0]
-    verify_program("""Counter (unsatisfiable)
+    verify_program(
+        False,
+        """Counter (unsatisfiable)
     Adds one to x as long as x < 64. Asserts that x remains less than 10.""",
-        variables, primes, And(x == 0), Or(And(xp == x + 1, x < 64), xp == x), x < 10)
+        variables,
+        primes,
+        And(x == 0),
+        Or(And(xp == x + 1, x < 64), xp == x),
+        x < 10,
+    )
+
 
 def counter_sat():
-    variables = [BitVec('x', 5)]
+    variables = [BitVec("x", 5)]
     x = variables[0]
-    primes = [BitVec('x\'', 5)]
+    primes = [BitVec("x'", 5)]
     xp = primes[0]
-    verify_program("""Counter (satisfiable)
+    verify_program(
+        True,
+        """Counter (satisfiable)
     Adds one to x as long as x < 7. Asserts that x remains less than 6.
     x is a 5 bit signed number, so it must only rule out 7 <= x <= 15.""",
-        variables, primes, And(x == 0), Or(And(xp == x + 1, x < 6), xp == x), x < 7)
+        variables,
+        primes,
+        And(x == 0),
+        Or(And(xp == x + 1, x < 6), xp == x),
+        x < 7,
+    )
+
 
 def add_sub_unsat():
-    variables = [BitVec('x', 6), BitVec('y', 6)]
+    variables = [BitVec("x", 6), BitVec("y", 6)]
     x, y = variables
-    primes = [BitVec('x\'', 6), BitVec('y\'', 6)]
+    primes = [BitVec("x'", 6), BitVec("y'", 6)]
     xp, yp = primes
     init = And(x == 4, y == 3)
     trans = And(xp == x + y, yp == x - y)
     post = Not(x == 32)
-    verify_program("""Addition and subtraction (unsatisfiable)
+    verify_program(
+        False,
+        """Addition and subtraction (unsatisfiable)
     x <- x + y, y <- x - y
     The values of x and y jump around and in frame 6, x = 32""",
-        variables, primes, init, trans, post)
+        variables,
+        primes,
+        init,
+        trans,
+        post,
+    )
+
 
 def add_sub_sat():
-    variables = [BitVec('x', 3), BitVec('y', 3)]
+    variables = [BitVec("x", 3), BitVec("y", 3)]
     x, y = variables
-    primes = [BitVec('x\'', 3), BitVec('y\'', 3)]
+    primes = [BitVec("x'", 3), BitVec("y'", 3)]
     xp, yp = primes
     init = And(x == 4, y == 3)
     trans = And(xp == x + y, yp == x - y)
     post = Not(x == 2)
-    verify_program("""Addition and subtraction (satisfiable)
+    verify_program(
+        True,
+        """Addition and subtraction (satisfiable)
     x <- x + y, y <- x - y
     The values of x and y jump around, but x can never hold the value 2""",
-        variables, primes, init, trans, post)
+        variables,
+        primes,
+        init,
+        trans,
+        post,
+    )
+
 
 def shifter_unsat():
     variables = generate_variables(4)
@@ -75,9 +120,17 @@ def shifter_unsat():
     init = And(*[var == False for var in variables[1:]])
     trans = And(*[primes[i] == variables[i - 1] for i in range(len(variables))])
     post = variables[-1] == False
-    verify_program("""Shifter (unsatisfiable)
+    verify_program(
+        False,
+        """Shifter (unsatisfiable)
         Starting with all but the LSB False, can the MSB remain False?""",
-        variables, primes, init, trans, post)
+        variables,
+        primes,
+        init,
+        trans,
+        post,
+    )
+
 
 def shifter_sat():
     variables = generate_variables(4)
@@ -85,51 +138,116 @@ def shifter_sat():
     init = variables[0]
     trans = And(*[primes[i] == variables[i - 1] for i in range(len(variables))])
     post = Or(*[var for var in variables])
-    verify_program("""Shifter (satisfiable)
+    verify_program(
+        True,
+        """Shifter (satisfiable)
     Starting with the LSB set to True, maintain at least one bit True""",
-        variables, primes, init, trans, post)
+        variables,
+        primes,
+        init,
+        trans,
+        post,
+    )
+
 
 def lights_out_sat():
     LEN = 9
     variables = generate_variables(LEN)
     primes = prime(variables)
-    on_bits = [0,1,2,5,6,7,8]
-    init = And(*([variables[i] for i in on_bits] + [Not(variable) for i, variable in enumerate(variables) if not i in on_bits]))
-    trans = Or([And(*[
-        (primes+primes)[j] == Not((variables+variables)[j]) if abs(j-i) <= 1 or abs(j-i) == LEN-1 else
-        (primes+primes)[j] == (variables+variables)[j] for j in range(LEN)]) for i in range(LEN)])
+    on_bits = [0, 1, 2, 5, 6, 7, 8]
+    init = And(
+        *([variables[i] for i in on_bits] + [Not(variable) for i, variable in enumerate(variables) if not i in on_bits])
+    )
+    trans = Or(
+        [
+            And(
+                *[
+                    (
+                        (primes + primes)[j] == Not((variables + variables)[j])
+                        if abs(j - i) <= 1 or abs(j - i) == LEN - 1
+                        else (primes + primes)[j] == (variables + variables)[j]
+                    )
+                    for j in range(LEN)
+                ]
+            )
+            for i in range(LEN)
+        ]
+    )
     post = Or(*[var for var in variables])
     verify_program(
+        True,
         """Lights out (satisfiable)
     Each step in the program represents a move of inverting a bit and its neighboring bits.
     For it to be unsolvable, neighboring bits must include wrapping around.
     Returning SAT means there is no solution to the starting conditions resulting in turning off all the bits.""",
-        variables, primes, init, trans, post, False, False)
+        variables,
+        primes,
+        init,
+        trans,
+        post,
+        False,
+        False,
+    )
+
 
 def lights_out_unsat():
     LEN = 9
     variables = generate_variables(LEN)
     primes = prime(variables)
-    on_bits = [0,1,2,5,6,7,8]
-    init = And(*([variables[i] for i in on_bits] + [Not(variable) for i, variable in enumerate(variables) if not i in on_bits]))
-    trans = Or([And(*[
-        (primes+primes)[j] == Not((variables+variables)[j]) if abs(j-i) <= 1 else
-        (primes+primes)[j] == (variables+variables)[j] for j in range(LEN)]) for i in range(LEN)])
+    on_bits = [0, 1, 2, 5, 6, 7, 8]
+    init = And(
+        *([variables[i] for i in on_bits] + [Not(variable) for i, variable in enumerate(variables) if not i in on_bits])
+    )
+    trans = Or(
+        [
+            And(
+                *[
+                    (
+                        (primes + primes)[j] == Not((variables + variables)[j])
+                        if abs(j - i) <= 1
+                        else (primes + primes)[j] == (variables + variables)[j]
+                    )
+                    for j in range(LEN)
+                ]
+            )
+            for i in range(LEN)
+        ]
+    )
     post = Or(*[var for var in variables])
     verify_program(
+        False,
         """Lights out (unsatisfiable)
     Each step in the program represents a move of inverting a bit and its neighboring bits.
     In this version neighboring bits do not wrap around.
     Returning UNSAT means there is a solution to the starting conditions resulting in turning off all the bits.""",
-        variables, primes, init, trans, post, show_trans = False)
+        variables,
+        primes,
+        init,
+        trans,
+        post,
+        show_trans=False,
+    )
+
 
 def run_all():
     [test() for test in tests[1:]]
-tests = [run_all, counter_sat, counter_unsat, shifter_sat, shifter_unsat, add_sub_sat, add_sub_unsat, lights_out_sat, lights_out_unsat]
+
+
+tests = [
+    run_all,
+    counter_sat,
+    counter_unsat,
+    shifter_sat,
+    shifter_unsat,
+    add_sub_sat,
+    add_sub_unsat,
+    lights_out_sat,
+    lights_out_unsat,
+]
 if __name__ == "__main__":
     test_lookup = {test.__name__: test for test in tests}
     if len(sys.argv) != 2:
-        print("Usage: python test_pdy.py [", ", ".join([test.__name__ for test in tests]), "]")
+        print("Usage: python test_pdy.py [{}]".format(", ".join([test.__name__ for test in tests])))
         exit()
 
     test_lookup[sys.argv[1]]()
